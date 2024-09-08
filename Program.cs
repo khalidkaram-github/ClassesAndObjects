a39d5987e329b0da5e8968e1cb776a6894ba142d
khalid karam <khalid.tasks@gmail.com>
2024-09-08 20:27:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class User
+    {
+        int age;
+        string name;
+
+
+        int GetAge() => age;
+        void SetAge(int _age) =>
+        age = _age;
+
+
+
+
+    }
+}
