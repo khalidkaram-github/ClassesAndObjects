77ec98aa395f1d5e3250b2c5130214dcd544cf2f
khalid karam <khalid.tasks@gmail.com>
2024-09-08 20:51:57 +0300

    ....

--- Program.cs
+++ Program.cs
@@ -5,21 +5,35 @@
         static void Main(string[] args)
         {
 
+            var user = new User();
+
+            //user.SetName("Eslam");
+            //user.SetAge(25);
+
+            Console.WriteLine($" name = {user.GetName()} - age = {user.GetAge()}");
         }
     }
 
-    class User
+
+}
+
+class User
+{
+    private int age;
+    public string name;
+
+
+    public User()
     {
-        int age;
-        string name;
-
-
-        int GetAge() => age;
-        void SetAge(int _age) =>
-        age = _age;
-
-
-
-
+        age = 10;
+        name = "Empty";
     }
+
+
+    public void SetAge(int _age) => age = _age;
+    public int GetAge() => age;
+
+    public void SetName(string _name) => name = _name;
+    public string GetName() => name;
+
 }
